f3d1cc0280cbf6d32398bb12ed9296f80d0a44cd
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 01:40:14 +0200

    enums-change-default-values

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 enum Role {
-  ADMIN,
+  ADMIN = 3,
   READ_ONLY,
   AUTHOR,
 }
 
 // By default admin=0, READ_ONLY=1, AUTHOR= 2
+// you can change those values by assign the first one, so
+// NOW admin=3, READ_ONLY=4, AUTHOR= 5
 
 let person = {
   name: "Maximilian",
