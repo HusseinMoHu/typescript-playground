41c7c8725e8c31f9885949f6e3d8e5f4f1c5275b
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 01:40:48 +0200

    enums

--- app.ts
+++ app.ts
@@ -1,12 +1,8 @@
 enum Role {
-  ADMIN = 3,
+  ADMIN,
   READ_ONLY,
   AUTHOR,
 }
-
-// By default admin=0, READ_ONLY=1, AUTHOR= 2
-// you can change those values by assign the first one, so
-// NOW admin=3, READ_ONLY=4, AUTHOR= 5
 
 let person = {
   name: "Maximilian",
