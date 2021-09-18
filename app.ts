350a19b9de1e44bd39e4630a8215102855678e2c
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 01:14:12 +0200

    Tuple-4

--- app.ts
+++ app.ts
@@ -13,4 +13,6 @@ let person: {
 person.role.push("admin"); // push has exception for no fuckin' reason!
 // person.role[1] = 10; !! Error: due to tuple
 
+person.role = [4, "admin"]; // this work fine. and respect tuple
+
 console.log(person);
