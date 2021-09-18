2cae28465df3f70ee28ecd838970b8fd625ea118
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-18 22:34:12 +0200

    Array types

--- app.ts
+++ app.ts
@@ -1,14 +1,18 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//   name: "Maximilian",
-//   age: 30,
-// };
-
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+// let favoriteActivities: any[];
+// favoriteActivities = ["Sports", 1];
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports", "Cooking"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLowerCase());
+  // console.log(hobby.map());  !! Error: Ts knows that hobby is string can't use mapMethod
+}
