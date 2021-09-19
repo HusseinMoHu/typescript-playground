1c63a0a08433057b1b45514341b1bd39496100b0
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 14:01:20 +0200

    Literal types

--- app.ts
+++ app.ts
@@ -1,19 +1,27 @@
-const combine = (input1: number | string, input2: number | string) => {
+const combine = (
+  input1: number | string,
+  input2: number | string,
+  resultConversion: string
+) => {
   let result;
-
-  // you may need runtime check when u work with union
-  // because union gives u a flexibility that may required a cost!
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
 
-  return result;
+  if (resultConversion === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
 };
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Hussein", "Mohamed");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Hussein", "Mohamed", "as-text");
 console.log(combinedNames);
