a2b58069701661eb29321d0bd71b7db6f96cc27c
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 15:55:39 +0200

    TypeScript detect func-type automatically

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 // Fun return number
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
 // Fun return void
-function printResult(num: number): void {
+function printResult(num: number) {
   console.log("Result: " + num);
 }
 
