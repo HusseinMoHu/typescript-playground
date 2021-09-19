e96947c9672a2b8096a9667cee424e48b41f6aa9
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 17:37:27 +0200

    Func-types & callbacks

--- app.ts
+++ app.ts
@@ -9,3 +9,13 @@ function printResult(num: number) {
 }
 
 printResult(add(1, 2));
+
+// Func-types & callbacks
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(20, 10, (result) => {
+  console.log(result);
+});
