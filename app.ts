7eb22c71edcc8d4b772a8267f265e5a043bf060a
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 12:18:57 +0200

    Any

--- app.ts
+++ app.ts
@@ -20,3 +20,14 @@ for (const hobby of person.hobbies) {
 if (person.role === Role.ADMIN) {
   console.log("is admin");
 }
+
+// any type
+let x: any;
+x = 10;
+x = true;
+x = ["string", 1];
+
+let y: any[];
+// y = 1;               !! Error
+// y "string";          !! Error
+y = [1, "t", 55, true];
