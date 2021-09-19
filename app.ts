424c15dd91d9782917b74b57daee8cb3e98e7245
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 15:54:20 +0200

    Return types & void

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
+// Fun return number
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
 
+// Fun return void
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
+
+printResult(add(1, 2));
