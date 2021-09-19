433c1fc8687e33e748433bc5fbc389c6a1bc94f5
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-19 12:56:15 +0200

    Union types

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
+const combine = (input1: number | string, input2: number | string) => {
+  let result;
 
+  // you may need runtime check when u work with union
+  // because union gives u a flexibility that may required a cost!
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  return result;
+};
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine("Hussein", "Mohamed");
+console.log(combinedNames);
