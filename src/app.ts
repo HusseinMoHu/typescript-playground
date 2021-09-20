5cd722f059e370b799f9e0755e538ff7908873e6
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-20 15:30:43 +0200

    Start Classes and interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,1 @@
-const button = document.querySelector("button")!;
-
-const clicked = {
-  message: "You are welcome",
-};
-
-function clickHandle(message: string) {
-  console.log(`Clicked!, ${message}`);
-}
-
-button.addEventListener("click", clickHandle.bind(null, "Hey!"));
+// Start classes
