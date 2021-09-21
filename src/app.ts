bb1f56e9789394c18c17fa91b038d55e31c8fa7a
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-09-21 22:00:31 +0200

    Init interface

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-// start interfaces
+interface Person {
+  name: string;
+  age: number;
+
+  great(phrase: string): void;
+}
+
+let user1: Person;
+
+user1 = {
+  name: "Hussein",
+  age: 24,
+  great(phrase: string) {
+    console.log(phrase + " " + this.name);
+  },
+};
+
+user1.great("Hi there - I am");
